cd6c52f7ae1a1f911145e77f862632fdba02703d
Nastija <nastija.sij@gmail.com>
2025-03-17 15:11:54 +0100

    added basic JS with typewriter

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  alert("Generating poem...");
+
+  new Typewriter("#generated-poem", {
+    strings: "Your Poem",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
